992e0dc2931ab7de217a1dcc56dfec28ea5a4b4b
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-26 17:10:27 -0300

    Implementada forma de separar a string das coordenadas em um int array

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -4,6 +4,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Informe as coordenadas limites do projeto (Ex.: 5 5 ou 8 4): ");
+        string[] partesCoordenadas = Console.ReadLine()!.Split();
+
+        int[] coordenadasMin = [0, 0];
+        int[] coordenadasMax = new int[partesCoordenadas.Length];
+
+        for (int i = 0; i < partesCoordenadas.Length; i++)
+            coordenadasMax[i] = int.Parse(partesCoordenadas[i]);
+
+        foreach (int i in coordenadasMax)
+            Console.WriteLine(i);
+
+        Console.ReadLine();
     }
 }
